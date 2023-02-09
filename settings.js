8709c06bf478f7775f34d522c2e98b533f930b6e
Kingsiyal524 <120108383+Kingsiyal524@users.noreply.github.com>
2023-02-09 17:31:04 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,20 +14,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
+global.owner = ['923252510140'] //ur owner number
+global.ownername = "𝐀𝐌𝐚𝐍_𝐒𝐢𝐲𝐚𝐥" //ur owner name
+global.ytname = "YT:  𝐀𝐌𝐚𝐍_Yt" //ur yt chanel name
 global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
+global.ownernomer = "923252410140" //ur number
+global.premium = ['923252410140'] //ur premium number
 global.botname = 'Xcoco' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
-global.packname = "Sticker By Xcoco" //ur sticker watermark packname
-global.author = "Xcoco" //ur sticker watermark author
-global.wm = "XCOCO BOT INC" //ur watermark
+global.packname = "Sticker By 𝐀𝐌𝐚𝐍_𝐒𝐢𝐲𝐚𝐥" //ur sticker watermark packname
+global.author = "AMaN" //ur sticker watermark author
+global.wm = "AMaN BOT INC" //ur watermark
 
 // Other
 global.sessionName = 'session'
